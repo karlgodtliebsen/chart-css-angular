172c6cfec3db6243a1e8004dc5df1baa5e931ef5
karlgodtliebsen <karl@godtliebsen.dk>
2021-03-22 00:11:55 +0100

    installed ngneat and eslint

--- projects/angular-chart-css/src/lib/chart.component.spec.ts
+++ projects/angular-chart-css/src/lib/chart.component.spec.ts
@@ -1,10 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Spectator, createComponentFactory } from '@ngneat/spectator';
+
 
 import { ChartComponent } from './chart.component';
 
 describe('ChartComponent', () => {
   let component: ChartComponent;
   let fixture: ComponentFixture<ChartComponent>;
+
+  let spectator: Spectator<ChartComponent>;
+  const createComponent = createComponentFactory(ChartComponent);
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,6 +24,19 @@ describe('ChartComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+  beforeEach(() => spectator = createComponent());
+/*
+  it('should have a success class by default', () => {
+    expect(spectator.query('button')).toHaveClass('success');
+  });
+
+  it('should set the class name according to the [className] input', () => {
+    spectator.setInput('className', 'danger');
+    expect(spectator.query('button')).toHaveClass('danger');
+    expect(spectator.query('button')).not.toHaveClass('success');
+  });
+*/
 
   it('should create', () => {
     expect(component).toBeTruthy();
